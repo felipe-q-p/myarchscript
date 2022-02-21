df4aeef3267e6252da4c58bd07f33b8ef3a6e1e7
felipe-q-p <96737700+felipe-q-p@users.noreply.github.com>
2022-02-21 23:46:24 +0000

    Update pt1.sh

--- pt1.sh
+++ pt1.sh
@@ -2,18 +2,18 @@ echo "enter the name for the partition (ex: /dev/sdd): "
 read disk
 echo "you entered $disk"
 cfdisk $disk
+fdisk -l
 mkfs.ext4 /dev/vda1
 mount /dev/vda1 /mnt
 pacstrap /mnt base linux linux-firmware base-devel nano
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 nano /etc/locale.gen
-locale-gen
 echo "choose a language from the list above"
-read lang
+read langlocale-gen
 echo "you entered $lang"
 echo LANG=$lang >> /etc/locale.conf
-cat /etc/vconsole.conf
+locale-gen
 echo "choose a keymap from the list above"
 read keymap
 echo "you entered $keymap"
@@ -29,5 +29,5 @@ passwd
 pacman -S grub networkmanager network-manager-applet dialog wireless_tools wpa_supplicant os-prober mtools dosfstools linux-headers
 grub-install --target=i386-pc /dev/vda
 grub-mkconfig -o /boot/grub/grub.cfg
-exit
-reboot
+
+
