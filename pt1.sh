9858852ff45a096d41e3b56304bf40ea0b1bfe83
felipe-q-p <96737700+felipe-q-p@users.noreply.github.com>
2022-02-21 22:11:11 +0000

    Update pt1.sh

--- pt1.sh
+++ pt1.sh
@@ -4,3 +4,18 @@ echo "you entered $disk"
 cfdisk $disk
 mkfs.ext4 /dev/vda1
 mount /dev/vda1 /mnt
+pacstrap /mnt base linux linux-firmware base-devel nano
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+cat /etc/locale.gen
+nano /etc/locale.gen
+locale-gen
+echo "choose a language from the list above"
+read lang
+echo "you entered $lang"
+echo LANG=$lang >> /etc/locale.conf
+cat /etc/vconsole.conf
+echo "choose a keymap from the list above"
+read keymap
+echo "you entered $keymap"
+echo KEYMAP=$keymap>> /etc/vconsole.conf
