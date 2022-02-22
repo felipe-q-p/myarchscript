3fd84e557fcf9ffde75de3864970ad307e7ba7da
felipe-q-p <96737700+felipe-q-p@users.noreply.github.com>
2022-02-22 12:12:13 +0000

    Create pt3.py

---
+++ pt3.py
@@ -0,0 +1,10 @@
+import os
+
+op = int(input("Are you using wifi?[S/N] ")).strip().upper()
+if op == 'S':
+  os.system(systemctl enable NetworkManager)
+  os.system(systemctl start NetworkManager)
+  os.system(nmtui)
+else:
+  os.system(systemctl enable NetworkManager)
+  os.system(systemctl start NetworkManager)
