918efc760c568ff08df350a2564535240971e425
felipe-q-p <96737700+felipe-q-p@users.noreply.github.com>
2022-02-22 12:36:25 +0000

    Update pt3.py

--- pt3.py
+++ pt3.py
@@ -8,3 +8,24 @@ if op == 'S':
 else:
   os.system('systemctl enable NetworkManager')
   os.system('systemctl start NetworkManager')
+user = str(input('Choose a user name: '))
+os.system(f'useradd -m -G whell {user}')
+os.system(f'passwd {user}')
+os.system('echo %wheel ALL=ALL(ALL:AL) ALL >> /etc/sudoers')
+driver = str(input("Are you using Intel, AMD or Nvidia? ")).strip().upper()
+if driver == 'INTEL':
+  os.system('sudo pacman -S xf86-video-intel')
+elif driver == 'AMD':
+  os.system('sudo pacman -S xf86-video-amdgpu')
+elif driver == 'NVIDIA':
+  os.system('sudo pacman -S nvidia nvidia-utils')
+
+os.system('sudo pacman -S xorg-server xorg-xinit')
+os.system('sudo pacman -S ttf-linux-libertine noto-fonts-emoji')
+os.system('pacman -S gdm')
+os.system('systemctl enable gdm')
+os.system('pacman -S gnome gnome-terminal nautilus gnome-tweaks gnome-control-center gnome-backgrounds adwaita-icon-theme')
+os.system('pacman -S flatpak')
+os.system('flatpak install io.gitlab.librewolf-community')
+reboot = str(input('Reboot now? [S/N]')).strip().upper()
+os.system('reboot')
