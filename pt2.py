66998e79a8395f57fecfb9471e67c72cc1d128a1
felipe-q-p <96737700+felipe-q-p@users.noreply.github.com>
2022-02-22 14:12:43 +0000

    Create pt2.py

--- pt2.py
+++ pt2.py
@@ -1,25 +1,20 @@
 import os
 
-lang =  str(input("choose a language from the list above"))
+lang =  str(input("choose a language: "))
 os.system(f'echo you entered {lang}')
-echo LANG=lang >> /etc/locale.conf
-nano /etc/locale.gen
-cd /mnt
-locale-gen
-pwd
-echo "choose a keymap from the list above"
-read keymap
-echo "you entered $keymap"
-echo KEYMAP=$keymap >> /etc/vconsole.conf
-echo "choose a hostname"
-read hostname
-echo "you entered $hostname"
-echo /etc/hostname $hostname
-echo /etc/hosts "127.0.0.1      localhost
+os.system(f'echo LANG={lang} >> /etc/locale.conf')
+os.system(pwd)
+keymap =  str(input("choose a keymap: ))
+os.system(f'echo you entered {keymap}')
+os.system(f'echo KEYMAP={keymap} >> /etc/locale.conf')
+hostname = str(input("choose a hostname: "))
+os.system(f'echo you entered {hostname}')
+os.system(f'echo {hostname} >> /etc/hostname')
+os.system(f'''  echo "127.0.0.1      localhost
 ::1            localhost
-127.0.1.1      $hostname.localdomain      $hostname"
-passwd
-pacman -S grub networkmanager network-manager-applet dialog wireless_tools wpa_supplicant os-prober mtools dosfstools linux-headers
-grub-install --target=i386-pc /dev/vda
-grub-mkconfig -o /boot/grub/grub.cfg
-echo "Please exit and reboot your system"
+127.0.1.1      {hostname}.localdomain      {hostname}" >> /etc/hosts''')
+os.system(passwd)
+os.system('pacman -S grub networkmanager network-manager-applet dialog wireless_tools wpa_supplicant os-prober mtools dosfstools linux-headers')
+os.system('grub-install --target=i386-pc /dev/vda')
+os.system('grub-mkconfig -o /boot/grub/grub.cfg')
+print("Please exit and reboot your system")
