1015962389728df3e3416acdef6f6084abb67ca5
felipe-q-p <96737700+felipe-q-p@users.noreply.github.com>
2022-02-22 16:52:33 +0000

    Update pt2.py

--- pt2.py
+++ pt2.py
@@ -1,19 +1,33 @@
 
 import os
+from time import sleep
 
 lang =  str(input("choose a language: "))
 os.system(f'echo you entered {lang}')
 os.system(f'echo LANG={lang} >> /etc/locale.conf')
+os.system(f'echo LANG={lang} >> /etc/locale.gen')
 os.system('pwd')
+os.system('cat /etc/locale.conf')
+sleep(5)
+os.system('locale.gen')
+sleep(5)
 keymap =  str(input("choose a keymap: "))
 os.system(f'echo you entered {keymap}')
 os.system(f'echo KEYMAP={keymap} >> /etc/locale.conf')
+os.system('cat /etc/vconsole.conf')
+sleep(5)
 hostname = str(input("choose a hostname: "))
 os.system(f'echo you entered {hostname}')
 os.system(f'echo {hostname} >> /etc/hostname')
+os.system('cat /etc/hostname')
+sleep(5)
+
 os.system(f'''  echo "127.0.0.1      localhost
 ::1            localhost
 127.0.1.1      {hostname}.localdomain      {hostname}" >> /etc/hosts''')
+os.system('cat /etc/hosts')
+sleep(5)
+
 os.system('passwd')
 os.system('pacman -S grub networkmanager network-manager-applet dialog wireless_tools wpa_supplicant os-prober mtools dosfstools linux-headers')
 os.system('grub-install --target=i386-pc /dev/vda')
