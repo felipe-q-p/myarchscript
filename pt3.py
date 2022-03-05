1a0a498545c1fd52d4d724e86f73e22aaa8b68c0
felipe-q-p <96737700+felipe-q-p@users.noreply.github.com>
2022-03-05 12:43:24 +0000

    Update pt3.py

--- pt3.py
+++ pt3.py
@@ -1,7 +1,7 @@
 import os
 from time import sleep
 
-op = str(input("Are you using wifi?[S/N] ")).strip().upper()
+op = str(input("\033[1;94mAre you using wifi?[S/N] ")).strip().upper()
 if op == 'S':
   os.system('systemctl start NetworkManager')
   os.system('nmtui')
@@ -10,7 +10,7 @@ else:
 user = str(input('Choose a user name: '))
 os.system(f'useradd -m -G wheel {user}')
 os.system(f'passwd {user}')
-driver = str(input("Are you using Intel, AMD or Nvidia? ")).strip().upper()
+driver = str(input("\033[1;96mAre you using Intel, AMD or Nvidia? ")).strip().upper()
 if driver == 'INTEL':
   print('\033[1;91mInstalling Intel drivers')
   sleep(2)
@@ -23,17 +23,17 @@ elif driver == 'NVIDIA':
   print('\033[1;91mInstalling Nvidia drivers')
   sleep(2)
   os.system('sudo pacman -S nvidia nvidia-utils')
-print('\033[1;91mInstalling Applications')
+print('\033[1;92mInstalling Applications')
 sleep(2)
 os.system('sudo pacman -S xorg-server xorg-xinit')
 os.system('sudo pacman -S --needed lightdm lightdm-gtk-greeter')
 os.system('sudo pacman -S efl enlightenment')
-os.system('pacman -S archlinux-wallpaper flatpak terminology kitty baobab gparted ephoto rage ecrire')
+os.system('pacman -S archlinux-wallpaper flatpak terminology kitty baobab gparted gthumb rage ecrire')
 os.system('flatpak install io.gitlab.librewolf-community')
 os.system('systemctl enable lightdm')
+os.system('sudo cp /myarchscript/kitty.conf /home/felipe/.config/kitty')
 os.system('nano /etc/sudoers')
 os.system('nano /etc/locale.gen')
-os.system('sudo cp /myarchscript/kitty.conf /home/felipe/.config/kitty')
 reboot = str(input('Reboot now? [Y/N]')).strip().upper()
 if reboot == 'Y':
   os.system('reboot')
